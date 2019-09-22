60b3bc47819df63cdf86763a5596435daa94abd2
qwsfree <freeqws@hotmail.com>
2019-09-22 23:09:22 +0800

    123

--- Assets/Scripts/playerCtrl.cs
+++ Assets/Scripts/playerCtrl.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class playerCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 screenPosition;//将物体从世界坐标转换为屏幕坐标
+    Vector3 mousePositionOnScreen;//获取到点击屏幕的屏幕坐标
+    Vector3 mousePositionInWorld;//将点击屏幕的屏幕坐标转换为世界坐标
+
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        MouseFollow();
+    }
+    void MouseFollow()
+    {
+        //获取鼠标在相机中（世界中）的位置，转换为屏幕坐标；
+        screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        //获取鼠标在场景中坐标
+        mousePositionOnScreen = Input.mousePosition;
+        //让场景中的Z=鼠标坐标的Z
+        mousePositionOnScreen.z = screenPosition.z;
+        //将相机中的坐标转化为世界坐标
+        mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+        //物体跟随鼠标移动
+        transform.position = mousePositionInWorld;
+        //物体跟随鼠标X轴移动
+        //transform.position = new Vector3(mousePositionInWorld.x, transform.position.y, transform.position.z);
     }
 }
