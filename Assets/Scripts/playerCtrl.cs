f83224e78b29693bd5245c4873f356841f8e5970
qwsfree <freeqws@hotmail.com>
2019-09-24 12:31:07 +0800

    鼠标跟随功能

--- Assets/Scripts/playerCtrl.cs
+++ Assets/Scripts/playerCtrl.cs
@@ -5,31 +5,43 @@ using UnityEngine;
 public class playerCtrl : MonoBehaviour
 {
     Vector3 screenPosition;//将物体从世界坐标转换为屏幕坐标
-    Vector3 mousePositionOnScreen;//获取到点击屏幕的屏幕坐标
-    Vector3 mousePositionInWorld;//将点击屏幕的屏幕坐标转换为世界坐标
+    Vector3 mousePositionOnScreen;//获取到鼠标的屏幕坐标
+    Vector3 mousePositionInWorld;//将鼠标的屏幕坐标转换为世界坐标
+
     public float MoveSpeed;
+    float Step;
+
+    Vector3 targetPosition;
+    
 
     void Start()
     {
-        MoveSpeed = 100.0f;
+        MoveSpeed = 5.0f;
     }
     void Update()
     {
-        MouseFollow();
+         MouseFollow();
     }
     void MouseFollow()
     {
+
+        Step = MoveSpeed * Time.deltaTime;
+
         //获取鼠标在相机中（世界中）的位置，转换为屏幕坐标；
         screenPosition = Camera.main.WorldToScreenPoint(transform.position);
         //获取鼠标在场景中坐标
-        mousePositionOnScreen = Input.mousePosition;
+         mousePositionOnScreen = Input.mousePosition;
         //让场景中的Z=鼠标坐标的Z
         mousePositionOnScreen.z = screenPosition.z;
         //将相机中的坐标转化为世界坐标
         mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+
+
+        //锁定y轴坐标
+        targetPosition = new Vector3(mousePositionInWorld.x, transform.position.y, transform.position.z);
         //物体跟随鼠标移动
-        transform.position+=mousePositionInWorld*MoveSpeed*Time.deltaTime;
-        //物体跟随鼠标X轴移动
-        //transform.position = new Vector3(mousePositionInWorld.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position,  targetPosition,Step);
+
+
     }
 }
