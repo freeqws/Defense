c7126a969972c207c9da21ccf9790aa24f9f311e
qwsfree <freeqws@hotmail.com>
2019-09-26 11:22:52 +0800

    player移动及射击的基本功能制作。

--- Assets/Scripts/playerCtrl.cs
+++ Assets/Scripts/playerCtrl.cs
@@ -7,41 +7,53 @@ public class playerCtrl : MonoBehaviour
     Vector3 screenPosition;//将物体从世界坐标转换为屏幕坐标
     Vector3 mousePositionOnScreen;//获取到鼠标的屏幕坐标
     Vector3 mousePositionInWorld;//将鼠标的屏幕坐标转换为世界坐标
+    public Vector3 targetPosition;
 
     public float MoveSpeed;
-    float Step;
 
-    Vector3 targetPosition;
-    
+    public static bool playerMoving;
 
     void Start()
     {
         MoveSpeed = 5.0f;
+        targetPosition.y = transform.position.y;
     }
     void Update()
     {
-         MouseFollow();
+        MouseFollow();
+
     }
     void MouseFollow()
-    {
-
-        Step = MoveSpeed * Time.deltaTime;
+    {       
 
         //获取鼠标在相机中（世界中）的位置，转换为屏幕坐标；
         screenPosition = Camera.main.WorldToScreenPoint(transform.position);
         //获取鼠标在场景中坐标
-         mousePositionOnScreen = Input.mousePosition;
+        mousePositionOnScreen = Input.mousePosition;
         //让场景中的Z=鼠标坐标的Z
         mousePositionOnScreen.z = screenPosition.z;
         //将相机中的坐标转化为世界坐标
         mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
-
-
         //锁定y轴坐标
-        targetPosition = new Vector3(mousePositionInWorld.x, transform.position.y, transform.position.z);
-        //物体跟随鼠标移动
-        transform.position = Vector3.MoveTowards(transform.position,  targetPosition,Step);
+        Vector3 mousePosition_x = new Vector3(mousePositionInWorld.x, transform.position.y, transform.position.z);
 
 
+        if (Input.GetMouseButton(0))
+        {
+            targetPosition = mousePosition_x;
+        }
+        //移动
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, MoveSpeed * Time.deltaTime);
+
+        //判断角色是否在移动
+        if (targetPosition.x != transform.position.x)
+        {
+            playerMoving = true;
+        }
+        else
+        {
+            playerMoving = false;
+        }
+                     
     }
 }
---
+++ Assets/Scripts/shootDefault.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootDefault : MonoBehaviour
+{
+    public GameObject bullet;
+    public float destroyBullet;
+    public float bulletSpeed;
+    private Rigidbody2D rb2d;
+
+    public float nextFire;
+    public float fireRate = 1;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        nextFire += Time.deltaTime;
+
+        if (playerCtrl.playerMoving == false && nextFire > fireRate)
+        {
+            GameObject m_bullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            rb2d = m_bullet.GetComponent<Rigidbody2D>();
+            rb2d.AddForce(transform.up * bulletSpeed);
+            Destroy(m_bullet, destroyBullet);
+            nextFire = 0;
+        }
+    }
+}
